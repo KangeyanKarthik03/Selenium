e3e70ab7606deb92c4489894b1bff29e592c6833
K Karthik <k221560@gmail.com>
2022-05-21 21:11:25 +0530

    Second Commit

--- sample/DemoA.java
+++ sample/DemoA.java
@@ -1,13 +1,14 @@
 package sample;
 
-public class DemoA {
+public class DemoA extends DemoB {
 
-	public static void main(String[] args) {
+	public void main(String[] args) {
 		System.out.println("Hello");
 		DemoA a = new DemoA();
 		a.test("Karthik");
+		super.demoB();
 	}
-	
+
 	public void test(String a) {
 		System.out.println("A value is"+a);
 	}
---
+++ sample/DemoB.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class DemoB {
+	
+	public void demoB() {
+		System.out.println("I am in Demo B");
+	}
+
+}
